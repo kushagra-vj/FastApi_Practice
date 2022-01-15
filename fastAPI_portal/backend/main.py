b2d0e81320c56287248c3a8f3af029f50aaa7038
kushagra-vj <kvj8295@gmail.com>
2022-01-15 19:59:23 +0530

    add routers for html pages and code for same

--- fastAPI_portal/backend/main.py
+++ fastAPI_portal/backend/main.py
@@ -1,9 +1,16 @@
 from fastapi import FastAPI
 from core.config import settings
+from apis.general_pages.route_homepage import general_pages_router
 
 
-app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+def include_router(app):
+    app.include_router(general_pages_router)
 
-@app.get('/')
-def first_api():
-    return {"msg":"Hello FAST API"}
+
+def start_application():
+    app = FastAPI(title=settings.PROJECT_NAME, version=settings.PROJECT_VERSION)
+    include_router(app)
+    return app
+
+
+app = start_application()
