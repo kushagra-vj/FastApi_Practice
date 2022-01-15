8a051b3cd85688db679f58a784b9406469e673f2
kushagra-vj <kvj8295@gmail.com>
2022-01-16 00:14:52 +0530

    string hasing done

---
+++ fastAPI_portal/backend/core/hashing.py
@@ -0,0 +1,15 @@
+import hashlib
+from hmac import compare_digest
+
+
+class Hasher():
+    
+    @staticmethod
+    def verify_password(plain_passowrd, hashed_password):
+        return hashlib.sha256(plain_passowrd.encode()).digest() == hashed_password
+
+    
+    @staticmethod
+    def get_password_hashed(password):
+        encoded = password.encode()
+        return hashlib.sha256(encoded).digest()
