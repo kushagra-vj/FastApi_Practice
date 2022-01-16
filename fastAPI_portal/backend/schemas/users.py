142498f9a0c0d94b55fea72359f0618c643314f5
kushagra-vj <kvj8295@gmail.com>
2022-01-16 13:57:31 +0530

    return request response  added

--- fastAPI_portal/backend/schemas/users.py
+++ fastAPI_portal/backend/schemas/users.py
@@ -1,9 +1,20 @@
 from lib2to3.pytree import Base
+from re import T
 from typing import Optional
 from pydantic  import BaseModel, EmailStr
 
 
+#properties required during user creation
 class UserCreate(BaseModel):
     username : str
     email : EmailStr
     password : str
+
+
+class ShowUserResponse(BaseModel):
+    username : str  
+    email : EmailStr
+    is_active : bool
+
+    class Config(): #tells pydantic to convert even non dict obj to json
+        orm_mode = True
