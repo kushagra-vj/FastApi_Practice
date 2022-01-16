c240ec2ff5a6f0185c161eb012e3447e53fe00a7
kushagra-vj <kvj8295@gmail.com>
2022-01-16 13:55:29 +0530

    code updated

--- fastAPI_portal/backend/core/hashing.py
+++ fastAPI_portal/backend/core/hashing.py
@@ -6,10 +6,10 @@ class Hasher():
     
     @staticmethod
     def verify_password(plain_passowrd, hashed_password):
-        return hashlib.sha256(plain_passowrd.encode()).digest() == hashed_password
+        return hashlib.sha256(plain_passowrd.encode()).hexdigest() == hashed_password
 
     
     @staticmethod
     def get_password_hashed(password):
         encoded = password.encode()
-        return hashlib.sha256(encoded).digest()
+        return hashlib.sha256(encoded).hexdigest()
