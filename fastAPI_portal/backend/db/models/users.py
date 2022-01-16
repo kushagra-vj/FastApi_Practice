76a050fc3dd87c44486c38a6329de6e411c8f830
kushagra-vj <kvj8295@gmail.com>
2022-01-16 13:56:30 +0530

    char field updated

--- fastAPI_portal/backend/db/models/users.py
+++ fastAPI_portal/backend/db/models/users.py
@@ -10,7 +10,7 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String(30), unique=True, nullable=False)
     email = Column(String(50), nullable=False, unique=True, index=True)
-    hashed_password = Column(String(16), nullable=False)
+    hashed_password = Column(String(100), nullable=False)
     is_active = Column(Boolean(), default=True)
     is_superuser = Column(Boolean(), default=False)
     jobs = relationship("Job", back_populates="owner")
